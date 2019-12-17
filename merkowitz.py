591395899d01239a8bbed91b9a9d8307e24bc820
John Mihalik <john@skyword.com>
2019-12-17 08:27:11 -0500

    Updates

--- merkowitz.py
+++ merkowitz.py
@@ -16,22 +16,28 @@ yf.pdr_override()
 #stocks = ['XLC', 'XLP', 'XLY', 'XLE', 'XLF', 'XLV', 'XLI', 'XLB', 'XLRE', 'XLK' , 'XLU']
 
 # SPY Industries
-stocks = ['KBE', 'KRE', 'KCE', 'KIE', 'XAR', 'XTN', 'XBI', 'XPH', 'XHE', 'XHS', 'XOP', 'XES', 'XME', 'XRT', 'XHB', 'XSD', 'XSW', 'XNTK', 'XITK', 'XTL', 'XTH' ,'XWEB']
+#stocks = ['KBE', 'KRE', 'KCE', 'KIE', 'XAR', 'XTN', 'XBI', 'XPH', 'XHE', 'XHS', 'XOP', 'XES', 'XME', 'XRT', 'XHB', 'XSD', 'XSW', 'XNTK', 'XITK', 'XTL', 'XTH' ,'XWEB']
 
 # SP500
 #stocks = ['LVS','NVR','CDW','LDOS','IEX','TMUS','MKTX','AMCR','CTVA','DOW','FOX','FOXA','WAB','ATO','TFX','FRC','CE','LW','MXIM','FANG','JKHY','KEYS','LIN','FTNT','ROL','WCG','ANET','CPRT','FLT','BR','HFC','TWTR','EVRG','ABMD','MSCI','TTWO','SIVB','IPGP','HII','NCLH','CDNS','SBAC','IQV','AOS','PKG','RMD','DRE','MGM','BHGE','HLT','ALGN','ANSS','RE','INFO','IT','DXC','RJF','ARE','AMD','SNPS','DISH','REG','CBOE','INCY','FTI','IDXX','MAA','COTY','COO','CHTR','MTD','FTV','ALB','LNT','FBHS','UA','TDG','AJG','LKQ','DLR','ALK','GPN','ULTA','CNC','HOLX','AWK','UDR','CXO','CFG','FRT','EXR','WLTW','CHD','nan','ILMN','SYF','HPE','VRSK','NWS','UAL','ATVI','PYPL','AAP','KHC','WRK','JBHT','QRVO','O','SLG','EQIX','HBI','AAL','HSIC','SWKS','HCA','RCL','UHS','URI','DISCK','MLM','AMG','XEC','AVGO','UAA','GOOG','ESS','TSCO','FB','MHK','ADS','ALLE','CPRI','VRTX','AME','DAL','NLSN','NWSA','ZTS','GM','KSU','MAC','REGN','PVH','ABBV','APTV','GRMN','DG','PNR','LYB','STX','MNST','LRCX','KMI','ALXN','PSX','CCI','TRIP','BWA','PRGO','DLTR','XYL','TEL','MOS','ACN','MPC','CMG','BLK','EW','FFIV','NFLX','IR','CB','KMX','CERN','OKE','DISCA','HP','BRK.B','NRG','ROP','ROST','V','BKNG','FMC','PWR','WDC','ORLY','ES','HRL','VTR','WELL','IRM','FLIR','RSG','XRAY','WYNN','PBCT','SJM','WEC','NDAQ','FLS','APH','PXD','LHX','CRM','FAST','CF','IVZ','DVA','MA','COG','ISRG','HCP','PM','AMT','JEC','EXPD','NBL','EXPE','ICE','MCHP','AKAM','DFS','AIZ','MDLZ','HST','CHRW','VAR','RL','AVB','CTSH','FIS','CBRE','CELG','WU','CME','JNPR','GOOGL','BXP','KIM','VRSN','EL','VIAB','AMZN','LEN','AMP','PSA','TSN','VNO','STZ','DHI','NOV','LH','TPR','GILD','VLO','MYL','ETFC','MTB','BIIB','PLD','SYMC','MKC','AIV','DGX','TRV','ANTM','UPS','GS','PRU','EA','EBAY','PFG','SPG','WAT','EQR','NVDA','ABC','ZBH','ZION','FISV','CTAS','SYK','MET','RHI','INTU','EOG','NI','MCO','DVN','TIF','SBUX','A','HOG','CTXS','XLNX','LEG','TROW','ADI','PNW','QCOM','VMC','BBY','NTAP','AFL','CMS','AGN','CTL','MCK','CCL','DHR','AES','PAYX','RF','KSS','COF','BEN','SEE','NTRS','OMC','CINF','BBT','YUM','KLAC','STT','HBAN','PGR','APA','EFX','SCHW','CAH','ADBE','TMO','AZO','AON','FITB','HIG','CMA','HUM','PPL','M','MS','FCX','ALL','DRI','L','BK','AMAT','BSX','CBS','MU','LUV','UNH','MSFT','KEY','UNM','EMN','CSCO','MAR','COST','IPG','AMGN','AEE','MRO','ADSK','ORCL','JCI','GL','NWL','ECL','NKE','STI','C','PNC','HD','AVY','CMCSA','MMC','SYY','HRB','MDT','JWN','GPS','ITW','PH','DOV','TJX','CNP','NOC','PKI','APD','NUE','BLL','HAS','LMT','HES','PHM','LOW','VZ','T','LB','CAG','OXY','AAPL','BF.B','SNA','SWK','WMT','WM','ADM','MAS','GWW','ADP','FDX','PCAR','AIG','WBA','VFC','TXT','TGT','INTC','TAP','BAC','WFC','DUK','LNC','AXP','CI','DIS','NEE','IFF','JPM','WMB','HPQ','GPC','JNJ','BAX','BDX','LLY','MCD','NEM','GIS','CLX','CSX','CMI','SLB','EMR','SHW','FE','WHR','XOM','SRE','DTE','EXC','WY','AEP','PG','XRX','ETR','CVS','PPG','NSC','UTX','ROK','DD','XEL','MSI','TXN','CL','HON','PFE','SPGI','CPB','GE','GD','K','MO','GLW','F','MMM','SO','IBM','CAT','ABT','MRK','RTN','BMY','ED','UNP','KO','ARNC','KR','HSY','HAL','COP','KMB','PEG','ETN','DE','CVX','EIX','IP','BA','D','PEP' ]
 
+# SPYDR Industrial ETF's AND Sectors
+#stocks = ['XLC', 'XLP', 'XLY', 'XLE', 'XLF', 'XLV', 'XLI', 'XLB', 'XLRE', 'XLK' , 'XLU', 'KBE', 'KRE', 'KCE', 'KIE', 'XAR', 'XTN', 'XBI', 'XPH', 'XHE', 'XHS', 'XOP', 'XES', 'XME', 'XRT', 'XHB', 'XSD', 'XSW', 'XNTK', 'XITK', 'XTL', 'XTH' ,'XWEB']
+
+stocks = ['SPYG', 'AAPL', 'WMT' , 'TSLA', 'GE']
 
 # Start and end date for stock data to use in calculations
-start = pd.to_datetime('2018-12-12')
-end = pd.to_datetime('2019-11-20')
+#start = pd.to_datetime('2017-12-06')
+#end = pd.to_datetime('2019-12-06')
+end = pd.Timestamp.today()
+start =  end -  datetime.timedelta(days=365)
 
-min_allocation = 0
-max_allocation = 0.3
+
+min_allocation = 0.0
+max_allocation = 1.0
 
 def download_data(stocks):
     data = pdr.get_data_yahoo(stocks, start=start, end=end) ['Adj Close']
-    data.columns = stocks
     return data
 
 def show_data(data):
@@ -57,7 +63,8 @@ def show_statistics(returns):
 
 # weights defines what stocks to include (with what portion) in the portfolio
 def initialize_weights():
-	weights = np.random.random(len(stocks))
+	weights = np.ones(len(stocks))
+#	weights = np.random.random(len(stocks))
 	weights /= np.sum(weights)
 	return weights
 
@@ -111,39 +118,38 @@ def statistics(weights, returns):
 	return np.array([portfolio_return, portfolio_volatility, portfolio_return / portfolio_volatility])
 
 
-# [2] means that we want to maximize according to the Sharpe-ration
-# note: maximizing f(x) function is the same as minimizing -f(x) !!!
 def min_func_sharpe(weights, returns):
 	return -statistics(weights, returns)[2]
 
 
-# what are the constraints? The sum of weights = 1 !!!  f(x)=0 this is the function to minimize
 def optimize_portfolio(weights, returns):
-	constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})  # the sum of weights is 1
-	bounds = tuple((min_allocation, max_allocation) for x in range(
-		len(stocks)))  # the weights can be 1 at most: 1 when 100% of money is invested into a single stock
-	optimum = optimization.minimize(fun=min_func_sharpe, x0=weights, args=returns, method='SLSQP', bounds=bounds,
-									constraints=constraints)
+
+	# the sum of weights is 1
+	constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+
+	# the weights can be 1 at most: 1 when 100% of money is invested into a single stock
+	bounds = tuple((min_allocation, max_allocation) for x in range(len(stocks)))
+
+	optimum = optimization.minimize(fun=min_func_sharpe, x0=weights, args=returns, method='SLSQP', bounds=bounds, constraints=constraints)
+
 	return optimum
 
 
 # optimal portfolio according to weights: 0 means no shares of that given company
 def print_optimal_portfolio(optimum, returns):
-	#print("Optimal weights:", optimum['x'].round(3))
-	#print("Expected return, volatility and Sharpe ratio:", statistics(optimum['x'].round(3), returns))
-
-	#print(tabulate([['Alice', 24], ['Bob', 19]], headers=['Name', 'Age'], tablefmt='orgtbl'))
 
 	expected_return, volatility, sharpe = statistics(optimum['x'].round(3), returns)
 
 	print("Portfolio Allocation:")
 	for i in range(0,len(stocks)):
-		print(" %6s : % 4.3f" % (stocks[i], optimum['x'][i]) )
+		if optimum['x'][i] > 0.00001:
+			print(" %6s : % 4.3f" % (returns.columns[i], optimum['x'][i]) )
 
 	print("\n-------------------------------------")
 	print("Expected Return  : ", expected_return.round(3))
 	print("Volatility       : ", volatility.round(3))
 	print("Sharpe Ratio     : ", sharpe.round(3))
+
 
 def show_optimal_portfolio(optimum, returns, preturns, pvariances):
 	plt.figure(figsize=(10, 7))
@@ -157,16 +163,17 @@ def show_optimal_portfolio(optimum, returns, preturns, pvariances):
 
 
 if __name__ == "__main__":
+
 	data = download_data(stocks)
-	show_data(data)
+#	show_data(data)
 	returns = calculate_returns(data)
 #	plot_daily_returns(returns)
 	show_statistics(returns)
 	weights=initialize_weights()
 #	calculate_portfolio_return(returns,weights)
 #	calculate_portfolio_variance(returns,weights)
-#	preturns, pvariances = generate_portfolios(10000, stocks, returns)
-#	plot_portfolios(preturns, pvariances)
+	preturns, pvariances = generate_portfolios(10000, stocks, returns)
+	plot_portfolios(preturns, pvariances)
 	optimum = optimize_portfolio(weights,returns)
 	print_optimal_portfolio(optimum, returns)
 #	show_optimal_portfolio(optimum, returns, preturns, pvariances)
